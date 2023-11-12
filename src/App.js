f454b5aa7df720685e4913f41382ddc9bd6a223b
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-11-12 04:04:53 +0100

    refactored

--- src/App.js
+++ src/App.js
@@ -4,29 +4,6 @@ import LeftSection from './components/leftSection';
 import { useState } from 'react';
 
 function App() {
- const [email, setEmail] = useState('');
- const [isValid, setIsValid] = useState(false);
- const [modal, setModal] = useState(false);
-
- const validateEmail = (e) => {
-  const regexEmailValidator = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-  e.preventDefault();
-  const valid = regexEmailValidator.test(e.target.value);
-  setIsValid(valid);
-  setEmail(e.target.value);
- };
-
- const handleSubmit = (e) => {
-  e.preventDefault();
-  console.log(email);
- };
-
- const openModal = (e) => {
-  e.preventDefault();
-  if (isValid && email !== '') setModal(true);
-  console.log('Nice');
- };
-
  return (
   <div className='main grid max-w-4xl lg:grid-cols-2  justify-items-center gap-10 p-4 rounded-3xl'>
    <LeftSection></LeftSection>
