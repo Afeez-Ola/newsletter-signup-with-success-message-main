0af168cfc89eb002abcdcb35c77d955f392e175c
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-11-12 17:32:51 +0100

    making the image responsive by using picture tag

--- src/components/rightSection.jsx
+++ src/components/rightSection.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import signUpIllustrationDesktop from '../assets/images/illustration-sign-up-desktop.svg';
+import signUpIllustrationMobile from '../assets/images/illustration-sign-up-mobile.svg';
 
 function RightSection() {
  return (
-  <div className='right'>
-   <img className='w-full h-full' src={signUpIllustrationDesktop} alt=''></img>
-  </div>
+  <picture className='right w-full'>
+   <source
+    media='(min-width: 900px)'
+    srcset={signUpIllustrationDesktop}
+   ></source>
+   <source
+    media='(min-width: 480px)'
+    srcset={signUpIllustrationMobile}
+   ></source>
+   <img
+    className='right w-full'
+    src={signUpIllustrationMobile}
+    alt='IfItDoesntMatchAnyMedia'
+   ></img>
+  </picture>
  );
 }
 
