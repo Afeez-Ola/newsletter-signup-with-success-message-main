4b341c542ff944151dcafd8cdf1679484c53e8a0
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-11-12 16:50:25 +0100

    edit the tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
  content: ['./public/**/*.{html,js}'],
  theme: {
   extend: {},
+  screens: {
+   sm: '375px',
+   // => @media (min-width: 576px) { ... }
+
+   md: '960px',
+   // => @media (min-width: 960px) { ... }
+
+   lg: '1440px',
+   // => @media (min-width: 1440px) { ... }
+  },
  },
  plugins: [],
 };
