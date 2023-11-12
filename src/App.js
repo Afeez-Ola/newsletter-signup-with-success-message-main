dc4b1fd9b1b308916b19dc5abff41af2ecdb2b27
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-11-12 03:44:32 +0100

    form validation done.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { useState } from 'react';
 function App() {
  const [email, setEmail] = useState('');
  const [isValid, setIsValid] = useState(false);
+ const [modal, setModal] = useState(false);
 
  const validateEmail = (e) => {
   const regexEmailValidator = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
@@ -20,11 +21,15 @@ function App() {
   console.log(email);
  };
 
+ const openModal = (e) => {
+  e.preventDefault();
+  if (isValid && email !== '') setModal(true);
+  console.log('Nice');
+ };
+
  return (
   <div className='main grid max-w-4xl grid-cols-2 gap-10 p-4 rounded-3xl'>
    <LeftSection
-    validateEmail={validateEmail}
-    handleSubmit={handleSubmit}
    ></LeftSection>
    <RightSection></RightSection>
   </div>
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Form(props) {
+ const [email, setEmail] = useState('');
+ const [isValid, setIsValid] = useState(false);
+ const [modal, setModal] = useState(false);
+
+ const validateEmail = (e) => {
+  const regexEmailValidator = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  e.preventDefault();
+  const input = e.target.value;
+  const valid = regexEmailValidator.test(input);
+  console.log(`valid is ${valid}`);
+  setIsValid(valid);
+  setEmail(e.target.value);
+ };
+
+ const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log(email);
+ };
+
+ const openModal = (e) => {
+  e.preventDefault();
+  if (isValid && email !== '') setModal(true);
+  console.log(email);
+ };
+
  return (
-  <form className='mail_section'>
+  <form
+   onSubmit={handleSubmit}
+   className={`mail_section ${isValid ? '' : 'formInvalid'}`}
+  >
    <label className='text-sm font-bold'>Email address</label>
-   <input 
-    onChange={props.validateEmail}
-    required
-    className={`p-3 rounded-md my-1 ${props.formIsValid}`}
+   <input
+    onChange={validateEmail}
+    className={`p-3 rounded-md my-1 ${isValid ? 'valid' : 'invalid'} `}
     type='text'
     placeholder='email@company.com'
    ></input>
-   <button
-    onSubmit={props.handleSubmit}
-    className='p-3 rounded-md my-4'
-    type='submit'
-   >
+   <button onClick={openModal} className='p-3 rounded-md my-4' type='submit'>
     Subscribe to monthly newsletter
    </button>
   </form>
